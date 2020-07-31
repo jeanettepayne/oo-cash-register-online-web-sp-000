664900839537c8f41793dcd935ff2fe117e672c1
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-31 01:00:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
+  attr_reader :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @title = title
+    @price = price
+    @quantity = quantity
+    @total = @total + @price * @quantity
+    
+    if @quantity > 1
+      counter = 0 
+      while counter < @quantity
+      @items << @title
+     counter += 1 
+   end 
+    else
+      @items << @title
+    end
+     
+  end
+  
+  def apply_discount
+    if @discount == 0 
+      "There is no discount to apply."
+    else
+      @total = @price - (@price * (@discount.to_f / 100))
+      @total = @total.to_i
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+  
+  def void_last_transaction
+    
+  end
 
+  
+end
